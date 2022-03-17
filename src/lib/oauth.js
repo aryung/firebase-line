d21e05693fb09866124979b22747c72914dde074
aryung.iMac <aryung@gmail.com>
2022-03-17 08:27:22 +0800

    remove file

--- src/lib/oauth.js
+++ src/lib/oauth.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin")
-const serviceAccount = require('./firebase-service-account-private-key.json')
+const serviceAccount = require('./firebase-private-key.json')
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_ENDPOINT
